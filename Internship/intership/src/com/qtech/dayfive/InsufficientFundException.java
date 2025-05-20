f22fdfde3e156b521fbe6db95043f84983a0529e
samus <samus@ESHWARI>
2025-05-20 15:58:05 +0530

    FifthCommit

--- Internship/intership/src/com/qtech/dayfive/InsufficientFundException.java
+++ Internship/intership/src/com/qtech/dayfive/InsufficientFundException.java
@@ -1,7 +1,18 @@
 package com.qtech.dayfive;
 
-public class InsufficientFundException extends Exception {
-	
-	public static void 
+public class InsufficientFundException extends Exception{
 
+	public InsufficientFundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsufficientFundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+		
+	
 }
